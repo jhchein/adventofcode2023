412f6801c9d8756872c6a563c69f8db309050e35
jhchein <hehein@microsoft.com>
2023-12-05 10:50:12 +0100

    updated day 5 pt 1 for clarity

--- 05/1.py
+++ 05/1.py
@@ -4,24 +4,29 @@ from tqdm import tqdm
 
 numbers = re.compile(r"(\d+)")
 
-
-def get_destination(sources, map):
-    map_lines = map.split("\n")[1:]
-    processed_map = {source: int(d) + (source - int(s)) for map_line in map_lines for source in sources
-                     for d, s, r in [numbers.findall(map_line)] if int(s) <= source <= int(s) + int(r) - 1}
+def map_destinations(sources: list, map: str) -> dict:
+    """Returns a mapping of sources to destinations ({source: destination}}])"""
+    map_lines = map.split("\n")[1:]                                                 # Remove first line (Title)
+    processed_map = {
+        source: int(destination_start) + (source - int(source_start))               # Create mapping: {source: destination + (source - start)}
+        for map_line in map_lines
+        for source in sources
+        for destination_start, source_start, range in [numbers.findall(map_line)]   # destination, source, range
+        if int(source_start) <= source <= int(source_start) + int(range) - 1        # if source is in range
+    }
     return processed_map
 
 
 with open("input.txt", "r") as f:
     lines = f.readlines()
 
-seeds = [int(seed) for seed in lines[0].split()[1:]]
-results = {seed: seed for seed in seeds}
+seeds = [int(seed) for seed in lines[0].split()[1:]]                # Seeds are the first line of the input
+results = {seed: seed for seed in seeds}                            # Initial Mapping: {seed: seed}
 
-for map in tqdm("".join(lines).split("\n\n")[1:], desc="Maps"):
-    mapping = get_destination(list(results.values()), map)
-    results = {k: mapping.get(v, v) for k, v in results.items()}
-    # print(f"Results: {results}")
+for map in tqdm("".join(lines).split("\n\n")[1:], desc="Maps"):     # Display progress bar for each map
+    sources = list(results.values())
+    mapping = map_destinations(sources, map)
+    results = {k: mapping.get(v, v) for k, v in results.items()}    # Get the new destination for each seed. If the source was not in the map, keep the old destination.
 
 print(f"Results: {results}")
 print(f"Shortest: {min(results.values())} for seed {min(results, key=results.get)}")
